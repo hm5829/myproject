6b0b5be1485ee17a1fd44369b793596b8f0de60b
JASON LIN <hn5829@yahoo.com.tw>
2017-03-10 15:10:48 +0800

    56565

---
+++ src/oo/obstraction/Bike.java
@@ -0,0 +1,8 @@
+package oo.obstraction;
+
+public class Bike {
+	String brand;
+	String name;
+	int price;
+	String type;
+}
---
+++ src/oo/obstraction/BikeTester.java
@@ -0,0 +1,9 @@
+package oo.obstraction;
+
+public class BikeTester {
+
+	public static void main(String[] args) {
+		Bike b1 = new Bike();
+	}
+
+}
