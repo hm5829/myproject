9147a3ebf7dbe680909512a4f0b5e0e7ecfc145f
hm5829 <hm5829@yahoo.com.tw>
2017-03-12 20:21:59 +0800

    Update Car.java

--- src/oo/obstraction/Car.java
+++ src/oo/obstraction/Car.java
@@ -5,5 +5,10 @@ public class Car {
   String name;
   int cc;
   String type;
-
+  public Car(String brand, String name, int cc, String type){
+          this.brand = new brand;
+          this.name = new name;
+          this cc = new cc;
+          this type = new type;
+  }
 }
