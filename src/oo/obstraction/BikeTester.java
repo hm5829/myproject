727484b239249d2508f39a48962d59f1d5cfa2f6
hm5829 <hm5829@yahoo.com.tw>
2017-03-20 19:22:51 +0800

    Update BikeTester.java

--- src/oo/obstraction/BikeTester.java
+++ src/oo/obstraction/BikeTester.java
@@ -3,7 +3,7 @@ package oo.obstraction;
 public class BikeTester {
 
 	public static void main(String[] args) {
-		Bike b1 = new Bike();
+		Bike b1 = new Bike("Giant", ESCAPE 1, 13800, Racing bicycle);
 	}
 
 }
