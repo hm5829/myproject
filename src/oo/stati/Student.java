eb5f3f6399659fc6769d29d1803e14915160fd6d
hm5829 <hm5829@yahoo.com.tw>
2017-04-25 16:30:55 +0800

    Update Student.java

--- src/oo/stati/Student.java
+++ src/oo/stati/Student.java
@@ -1,5 +1,30 @@
 package oo.stati;
 
 public class Student {
-
+	int english;
+	int math;
+	int chinese;
+	static int pass = 60;
+	public Student(){
+		
+	}
+	public Student(int english, int math, int chinese) {
+		super();
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+	}
+	
+	public void print(){
+		System.out.println(getScore(english)+"\t"
+			+getScore(math)+"\t"+getScore(chinese));
+	}
+	
+	private String getScore(int score){
+		if (score < pass){
+			return String.valueOf(score)+"*";
+		}else{
+			return String.valueOf(score);
+		}
+	}
 }
