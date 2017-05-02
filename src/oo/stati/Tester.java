cd0c49f266342970ee325e3ba1bd3da95d61b65a
user <user@es302n34>
2017-05-02 15:02:28 +0800

    125

--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -3,8 +3,13 @@ package oo.stati;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(Student.pass);
+		Student stu1 = new Student(58, 98, 66);
+		Student stu2 = new Student(67, 56, 77);
+		stu1.pass = 70;
+		stu1.print();
+		stu2.print();
+		
 
 	}
-
 }
