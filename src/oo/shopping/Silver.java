f4e4ac8a00f76ae7a55afd63f3af10c9236506c4
hm5829 <hm5829@yahoo.com.tw>
2017-05-16 14:21:13 +0800

    Update Silver.java

--- src/oo/shopping/Silver.java
+++ src/oo/shopping/Silver.java
@@ -1,7 +1,7 @@
 package oo.shopping;
 
 public class Silver extends Customer {
-	public void pay(int price) {
+	public void discount(int price) {
 		discount = price * 9 / 10;
 		System.out.println("銀級應付金額" + pay);
 	}
