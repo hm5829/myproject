64166027b94bb51772f079b25b011f86eec0ef82
hm5829 <hm5829@yahoo.com.tw>
2017-05-16 14:21:01 +0800

    Update Gold.java

--- src/oo/shopping/Gold.java
+++ src/oo/shopping/Gold.java
@@ -1,8 +1,8 @@
 package oo.shopping;
 
 public class Gold extends Customer{
-	public void pay(int price) {
-		pay = price * 9 / 10;
+	public void discount(int price) {
+		discount = price * 9 / 10;
 		System.out.println("金級應付金額" + pay);
 	}
 
