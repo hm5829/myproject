13b465762f1d93c9db6582d23251f85f72ca8247
user <user@es302n34>
2017-05-23 16:30:14 +0800

    abc

--- src/oo/io/SimpleWrite.java
+++ src/oo/io/SimpleWrite.java
@@ -1,26 +1,22 @@
 package oo.io;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class SimpleWrite {
 
 	public static void main(String[] args) {
-//		FileOutputStream fos =
-//				new FileOutputStream("data.txt");
-		int sum = 0;
-		Scanner scanner = new Scanner(System.in);
-	   for (int i = 0; i < 3; i++){
-		   System.out.println("請輸入成績:");
-		   String data = scanner.nextLine();
-		   try{
-			   int score = Integer.parseInt(data);
-			   sum = sum + score;
-		   }catch(NumberFormatException e){
-			   System.out.println("成績格式不符合規定:");
-			   i--;
-		   }
+		try{
+			FileOutputStream fos = new FileOutputStream("data.txt");
+			fos.write(98);
+			fos.flush();
+			fos.close();
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
-	   System.out.println(sum);
-}
+
 }
