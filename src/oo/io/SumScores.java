8d14efc3601f25b804af7ee44529e68ea8fa4465
user <user@es302n34>
2017-06-06 16:31:10 +0800

    123

--- src/oo/io/SumScores.java
+++ src/oo/io/SumScores.java
@@ -3,7 +3,7 @@ package oo.io;
 import java.io.FileOutputStream;
 import java.util.Scanner;
 
-public class SimpleWrite {
+public class SumScores {
 
 	public static void main(String[] args) {
 //		FileOutputStream fos =
@@ -11,13 +11,13 @@ public class SimpleWrite {
 		int sum = 0;
 		Scanner scanner = new Scanner(System.in);
 	   for (int i = 0; i < 3; i++){
-		   System.out.println("請輸入成績:");
+		   System.out.println("隢撓���蜀:");
 		   String data = scanner.nextLine();
 		   try{
 			   int score = Integer.parseInt(data);
 			   sum = sum + score;
 		   }catch(NumberFormatException e){
-			   System.out.println("成績格式不符合規定:");
+			   System.out.println("��蜀�撘�泵�����:");
 			   i--;
 		   }
 	}
--- src/oo/shopping/Customer.java
+++ src/oo/shopping/Customer.java
@@ -1,16 +1,13 @@
 package oo.shopping;
 
 public class Customer {
-	int price;
-	int discount;
-	int payback;
-		public void discount(int price) {
-			discount = price;
-			System.out.println("應付金額" + pay);
-		}
-		public void payback(int price) {
-			payback = 0;
-			System.out.println("回饋金" + payback);
-		}
+	int amount;
+
+	public Customer(int amount){
+		this.amount = amount;
+	}
 	
+	public void print(){
+		System.out.println(amount+"\t"+amount+"\t0");
+	}
 }
